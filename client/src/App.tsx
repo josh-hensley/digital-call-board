240623f1e082c9d89911388de58ebbc7953d2c03
josh-hensley <me@joshhensley.com>
2025-04-18 18:12:23 -0500

    created scaffolding for project, backend and frontend folders, made a simple callboard page. No functionality yet.

---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+function App() {
+  return (
+    <>
+      <nav>
+        <h1>RSM Shrek The Musical</h1>
+        <ul>
+          <li><a href="/">Callboard</a></li>
+          <li><a href="/contacts">Contacts</a></li>
+          <li><a href="/reports">Reports</a></li>
+          <li><a href="/downloads">Downloads</a></li>
+        </ul>
+      </nav>
+      <main>
+        <form>
+          <h2>Callboard</h2>
+          <textarea name="post" id="post-textarea" defaultValue="Post something to the callboard..."></textarea>
+          <button type="submit">Post</button>
+        </form>
+        <section id="posts">
+          <div className="post">
+            <h3>Name</h3>
+            <p>Post content goes here...</p>
+            <p className="timestamp">Posted on: YYYY-MM-DD</p>
+            <button type="button">Edit</button>
+            <button type="button">Delete</button>
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/main.tsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
+
+import './index.css'
+import App from './App.tsx'
+import Callboard from './pages/Callboard.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <div>Oops, Something went wrong!</div>,
+    children: [
+      {
+        index: true,
+        element: <Callboard />
+      }, {
+        path: '/contacts', 
+        element: <div>Contacts</div>
+      }, {
+        path: '/reports',
+        element: <div>Reports</div>
+      }, {
+        path: '/downloads',
+        element: <div>Downloads</div>
+      }
+    ]
+  }
+]);
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(<RouterProvider router={router} />)
+}
