86af2b6632470b4e88f0a826d50434cfe5eab93e
josh-hensley <me@joshhensley.com>
2025-05-16 15:57:55 -0500

    added storage persistance to create report

--- client/src/pages/CreateReport.tsx
+++ client/src/pages/CreateReport.tsx
@@ -1,5 +1,5 @@
 import { useQuery, useMutation } from "@apollo/client";
-import { ChangeEvent, FormEvent, useState } from "react"
+import { ChangeEvent, FormEvent, useState, useEffect } from "react"
 import { QUERY_USERS } from "../utils/queries";
 import { ADD_REPORT } from "../utils/mutations";
 import UserProps from "../interfaces/UserProps";
@@ -27,6 +27,17 @@ export default function CreateReport() {
         scenery: ""
     })
 
+    useEffect(() => {
+        if (localStorage.getItem('report')) {
+            const report = JSON.parse(localStorage.getItem('report') || '{}')
+            setFormState(report)
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('report', JSON.stringify(formState))
+    }, [formState])
+
     const calcRehearsalTime = (start: string, break1: string, break2: string, end: string) => {
         const endHour = parseInt(end.split(':')[0]);
         const endMinute = parseInt(end.split(':')[1]);
@@ -48,8 +59,8 @@ export default function CreateReport() {
 
     const handleAttendance = (e: ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
-        const attendance = formState.attendance
-        setFormState({ ...formState, attendance: value == "on" ? [...attendance, name] : attendance.splice(attendance.indexOf(name as never), 1) })
+        const attendance = formState.attendance;
+        setFormState({ ...formState, attendance: value == 'on' ? [...attendance, name] : attendance.filter((attendee) => attendee !== name) })
     }
 
     const handleSubmit = async (e: FormEvent) => {
@@ -76,19 +87,19 @@ export default function CreateReport() {
                             <fieldset className="col-2 bg-semi-transparent m-1 rounded">
                                 <legend>Time Card</legend>
                                 <label htmlFor="date">Date: </label>
-                                <input className='form-control' type="date" name="date" onChange={handleChange} value={formState.date}/>
+                                <input className='form-control' type="date" name="date" onChange={handleChange} value={formState.date} />
                                 <label htmlFor="rehearsalStart">Rehearsal Start: </label>
-                                <input className="form-control" type="time" name="rehearsalStart" onChange={handleChange} value={formState.rehearsalStart}/>
+                                <input className="form-control" type="time" name="rehearsalStart" onChange={handleChange} value={formState.rehearsalStart} />
                                 <label htmlFor="break1">Break: </label>
                                 <input className="form-control" type="time" name="break1" onChange={handleChange} value={formState.break1} />
                                 <label htmlFor="breakLength1">Break Length: {formState.breakLength1}</label>
-                                <input className="form-control" type="range" min='0' max='20' name="breakLength1" onChange={handleChange} value={formState.breakLength1}/>
+                                <input className="form-control" type="range" min='0' max='20' name="breakLength1" onChange={handleChange} value={formState.breakLength1} />
                                 <label htmlFor="break2">Break: </label>
-                                <input className="form-control" type="time" name="break2" onChange={handleChange} value={formState.break2}/>
+                                <input className="form-control" type="time" name="break2" onChange={handleChange} value={formState.break2} />
                                 <label htmlFor="breakLength2">Break Length: {formState.breakLength2}</label>
-                                <input className="form-control" type="range" min='0' max='20' name="breakLength2" onChange={handleChange} value={formState.breakLength2}/>
+                                <input className="form-control" type="range" min='0' max='20' name="breakLength2" onChange={handleChange} value={formState.breakLength2} />
                                 <label htmlFor="rehearsalEnd">Rehearsal End: </label>
-                                <input className="form-control" type="time" name="rehearsalEnd" onChange={handleChange} value={formState.rehearsalEnd}/>
+                                <input className="form-control" type="time" name="rehearsalEnd" onChange={handleChange} value={formState.rehearsalEnd} />
                                 <p>Total Rehearsal Time:</p>
                                 <p>{calcRehearsalTime(formState.rehearsalStart, formState.breakLength1, formState.breakLength2, formState.rehearsalEnd)}</p>
                             </fieldset>
@@ -100,7 +111,7 @@ export default function CreateReport() {
                                             users.map((user: UserProps) => {
                                                 return (
                                                     <li key={user._id}>
-                                                        <input className="form-check-input" type="checkbox" name={user.name} onChange={handleAttendance} />
+                                                        <input className="form-check-input" type="checkbox" name={user.name} onChange={handleAttendance} checked={formState.attendance.includes(user.name)} />
                                                         <label className='form-check-label px-2' htmlFor={user.name}>{user.name}</label>
                                                     </li>
                                                 )
@@ -122,23 +133,26 @@ export default function CreateReport() {
                     <fieldset className="container bg-semi-transparent rounded p-1">
                         <legend>Notes</legend>
                         <label htmlFor="rehearsal-notes">Rehearsal Notes: </label>
-                        <textarea className="form-control" name="rehearsalNotes" onChange={handleChange}></textarea>
+                        <textarea className="form-control" name="rehearsalNotes" onChange={handleChange} value={formState.rehearsalNotes}></textarea>
                         <label htmlFor="costumes">Costumes: </label>
-                        <textarea className="form-control" name="costumes" onChange={handleChange}></textarea>
+                        <textarea className="form-control" name="costumes" onChange={handleChange} value={formState.costumes}></textarea>
                         <label htmlFor="lights">Lights: </label>
-                        <textarea className="form-control" name="lights" onChange={handleChange}></textarea>
+                        <textarea className="form-control" name="lights" onChange={handleChange} value={formState.lights}></textarea>
                         <label htmlFor="properties">Properties: </label>
-                        <textarea className="form-control" name="properties" onChange={handleChange}></textarea>
+                        <textarea className="form-control" name="properties" onChange={handleChange} value={formState.properties}></textarea>
                         <label htmlFor="sound">Sound: </label>
-                        <textarea className="form-control" name="sound" onChange={handleChange}></textarea>
+                        <textarea className="form-control" name="sound" onChange={handleChange} value={formState.sound}></textarea>
                         <label htmlFor="scenery">Scenery: </label>
-                        <textarea className="form-control" name="scenery" onChange={handleChange}></textarea>
+                        <textarea className="form-control" name="scenery" onChange={handleChange} value={formState.scenery}></textarea>
                     </fieldset>
                     {
                         error ? (<div className="text-light">An Error has occured</div>) :
                             mutationData ? (<div className="text-light">Success</div>) :
                                 (
-                                    <button className="btn btn-primary w-10 m-3" style={{width: '100px'}} type="submit">Submit</button>
+                                    <div>
+                                        <button className="btn btn-primary w-10 m-3" style={{ width: '100px' }} type="submit">Submit</button>
+                                        <button className="btn btn-primary w-10 m-3" style={{ width: '150px' }} onClick={() => { localStorage.removeItem('report') }}>Reset Form</button>
+                                    </div>
                                 )}
 
                 </form>
