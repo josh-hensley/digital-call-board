8f39df2048148728e70b1cfad4ecfacdd1582212
josh-hensley <me@joshhensley.com>
2025-05-25 15:41:48 -0500

    fixed ugly videos page

--- client/src/pages/Videos.tsx
+++ client/src/pages/Videos.tsx
@@ -7,12 +7,10 @@ export default function Downloads() {
             {Auth.loggedIn() ? (
                 <div className='container p-3'>
                     <h2 className='text-light text-center'>Videos</h2>
-                    <section className='d-flex justify-content-around m-2'>
+                    <section className='d-flex justify-content-around m-2 flex-wrap'>
                         <div className="card p-3 d-flex align-items-center">
                             <h3>Story of My Life</h3>
-                            <iframe 
-                            width="560" 
-                            height="315" 
+                            <iframe  
                             src="https://www.youtube.com/embed/ONYG95b5_4M?si=7IVKb0qWEBiZYEOy" 
                             title="YouTube video player" 
                             frameBorder="0" 
@@ -24,8 +22,6 @@ export default function Downloads() {
                         <div className="card p-3 d-flex align-items-center">
                             <h3>Duloc</h3>
                             <iframe 
-                            width="560" 
-                            height="315" 
                             src="https://www.youtube.com/embed/x1WDnmBFDlk?si=GbD3OFUv8d0sNobY" 
                             title="YouTube video player" 
                             frameBorder="0" 
@@ -37,8 +33,6 @@ export default function Downloads() {
                         <div className="card p-3 d-flex align-items-center">
                             <h3>Forever</h3>
                             <iframe 
-                            width="560" 
-                            height="315" 
                             src="https://www.youtube.com/embed/KYZZS2XeI6g?si=yiwaW0r1Zy_CFOsR" 
                             title="YouTube video player" 
                             frameBorder="0" 
