30c6e38271b27297c2578d4150532db78d1dc51e
josh-hensley <me@joshhensley.com>
2025-09-06 14:48:34 -0500

    made small adjustment to connection

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -5,6 +5,13 @@ import { Sequelize } from 'sequelize';
 
 const { DB_URI } = process.env;
 
-const db = new Sequelize(DB_URI || '');
+const db = new Sequelize(DB_URI || '', {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true
+    }
+  }
+});
 
 export default db;
